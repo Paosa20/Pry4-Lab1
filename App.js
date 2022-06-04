01b7c238fab2ee74da31018d6d3d726e3d005009
Maria Sanchez <paosanchez30@hotmail.com>
2022-06-03 23:06:56 -0600

    today's tasks section plus styles

--- App.js
+++ App.js
@@ -4,10 +4,17 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello im working</Text>
 
-      
-      <StatusBar style="auto" />
+      {/* {today tasks} */}
+  <View style={styles.tasksWrapper}>
+
+
+    <Text style={styles.sectionTitle}> Today's tasks</Text>
+
+    <View style={styles.items}> 
+    {/* {tasks will go here} */}
+    </View>
+  </View>
     </View>
   );
 }
@@ -15,8 +22,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper:  {
+    paddingTop: 80, 
+    paddingHorizontal: 20, 
+  },
+sectionTitle: {
+  fontSize: 24, 
+  fontWeight: 'bold', 
+},
+items: {}
 });

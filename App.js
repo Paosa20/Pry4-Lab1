f874336116542c621677072a7e28a2f6b4632661
Maria Sanchez <paosanchez30@hotmail.com>
2022-06-03 23:30:34 -0600

    added new task component and last style

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Task from './components/Task';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Task from "./components/Task";
 
 export default function App() {
   return (
     <View style={styles.container}>
-
       {/* {today tasks} */}
-  <View style={styles.tasksWrapper}>
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}> Today's tasks</Text>
 
-
-    <Text style={styles.sectionTitle}> Today's tasks</Text>
-
-    <View style={styles.items}> 
-
-    <Task text={"hi"}/> 
-    {/* {tasks will go here} */}
-    </View>
-  </View>
+        <View style={styles.items}>
+          {/* {tasks will go here} */}
+          <Task text={"First task"} />
+          <Task text={"Second task"} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -25,15 +22,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E8EAED',
+    backgroundColor: "#E8EAED",
   },
-  tasksWrapper:  {
-    paddingTop: 80, 
-    paddingHorizontal: 20, 
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
   },
-sectionTitle: {
-  fontSize: 24, 
-  fontWeight: 'bold', 
-},
-items: {}
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
+  },
 });
